f70659da3fc5092f93bbe99d09d9db860c7b967a
Raymond Bian <raybbian@gmail.com>
2023-11-24 14:43:40 -0500

    fixed api url, sort listings

--- ui/src/components/Calendar.js
+++ ui/src/components/Calendar.js
@@ -17,7 +17,7 @@ export default function Calendar({startDay, roomID, selections, setSelections}) 
         // const todayString = dayRef.current.toLocaleDateString('en-CA')
         const todayString = new Date(dayRef.current.getTime() + startDay * 24 * 60 * 60 * 1000).toLocaleDateString('en-CA') //gets date in yyyy-mm-dd format
         const nextWeekString = new Date(dayRef.current.getTime() + (startDay + 7) * 24 * 60 * 60 * 1000).toLocaleDateString('en-CA')
-        axios.get(`http://api.multibooker.raybb.dev/get-rooms?from_date_string=${todayString}&to_date_string=${nextWeekString}`).then((res) => {
+        axios.get(`https://api.multibooker.raybb.dev/get-rooms?from_date_string=${todayString}&to_date_string=${nextWeekString}`).then((res) => {
             const out = {}
             res.data["slots"].forEach((room) => {
                 const startTime = new Date(room["start"]).toISOString()
--- ui/src/components/SideBar.js
+++ ui/src/components/SideBar.js
@@ -4,7 +4,7 @@ export default function SideBar({selections}) {
     return (
         <div className={"w-full h-full bg-cream shadow-xl -z-10 p-8 flex flex-col gap-4"}>
             <p className={"text-2xl font-bold"}>Current Bookings:</p>
-            {Object.keys(selections).reverse().map((date) => (
+            {Object.keys(selections).sort().map((date) => (
                 <div className={""}>
                     <p className={"text-xl font-semibold"}>{new Date(date).toDateString()}</p>
                     {selections[date].map((booking) => (

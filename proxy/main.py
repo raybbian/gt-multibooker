c9e0541ba0867c9863dc7f745e86a20ea85a4d8f
Raymond Bian <raybbian@gmail.com>
2023-11-25 02:20:06 -0500

    final merge, up to date

--- proxy/main.py
+++ proxy/main.py
@@ -34,7 +34,7 @@ def get_rooms(from_date_string, to_date_string, page_index=0, page_size=18):
 
 
 @app.post('/add-room')
-def add_room(item_id, start_time_urlencoded, duration: int, checksum, from_date, to_date, lc_ea_po):
+def add_room(item_id, start_time_urlencoded, duration: int, checksum, from_date, to_date, lc_ea_po, name, additional_access, gtid_list):
     url = "https://libcal.library.gatech.edu/spaces/availability/booking/add"
     payload = f'add%5Beid%5D={item_id}&add%5Bgid%5D={GID}&add%5Blid%5D={LID}&add%5Bstart%5D={start_time_urlencoded}&add%5Bchecksum%5D={checksum}&lid={LID}&gid={GID}&start={from_date}&end={to_date}'
     headers = {
@@ -43,18 +43,21 @@ def add_room(item_id, start_time_urlencoded, duration: int, checksum, from_date,
     }
     response = requests.request("POST", url, headers=headers, data=payload)
     data = json.loads(response.text)
-    id = data['bookings'][0]['id']
+    booking_id = data['bookings'][0]['id']
     seat_id = data['bookings'][0]['seat_id']
     new_checksum = data['bookings'][0]['checksum']
     option_selected = data['bookings'][0]['optionSelected']
     prev_end = data['bookings'][0]['options'][option_selected].replace(' ', '.')
     update_end = data['bookings'][0]['options'][duration - 1].replace(' ', '.')
     update_checksum = data['bookings'][0]['optionChecksums'][duration - 1]
-    payload = f'bookings%5B0%5D%5Bid%5D={id}&bookings%5B0%5D%5Beid%5D={item_id}&bookings%5B0%5D%5Bgid%5D={GID}&bookings%5B0%5D%5Bseat_id%5D={seat_id}&bookings%5B0%5D%5Blid%5D={LID}&bookings%5B0%5D%5Bstart%5D={start_time_urlencoded}&bookings%5B0%5D%5Bend%5D={prev_end}&bookings%5B0%5D%5Bchecksum%5D={new_checksum}&lid={LID}&gid={GID}&start={from_date}&end={to_date}&update%5Bid%5D={id}&update%5Bchecksum%5D={update_checksum}&update%5Bend%5D={update_end}'
+    payload = f'bookings%5B0%5D%5Bid%5D={booking_id}&bookings%5B0%5D%5Beid%5D={item_id}&bookings%5B0%5D%5Bgid%5D={GID}&bookings%5B0%5D%5Bseat_id%5D={seat_id}&bookings%5B0%5D%5Blid%5D={LID}&bookings%5B0%5D%5Bstart%5D={start_time_urlencoded}&bookings%5B0%5D%5Bend%5D={prev_end}&bookings%5B0%5D%5Bchecksum%5D={new_checksum}&lid={LID}&gid={GID}&start={from_date}&end={to_date}&update%5Bid%5D={booking_id}&update%5Bchecksum%5D={update_checksum}&update%5Bend%5D={update_end}'
     next_response = requests.request("POST", url, headers=headers, data=payload)
     next_data = json.loads(next_response.text)
     new_checksum = next_data['bookings'][0]['checksum']
-    return create_cart(item_id, start_time_urlencoded, update_end, new_checksum, lc_ea_po)
+    cart_data = create_cart(item_id, start_time_urlencoded, update_end, new_checksum, lc_ea_po)
+    lc_ebcart = cart_data['cartCookie']
+    session_id = cart_data['sessionId']
+    return submit(name, additional_access, gtid_list, session_id, lc_ea_po, lc_ebcart)
 
 
 def create_cart(item_id, start_time_urlencoded, end_time_urlencoded, checksum, lc_ea_po):
@@ -77,9 +80,6 @@ def create_cart(item_id, start_time_urlencoded, end_time_urlencoded, checksum, l
     return {"message": json.loads(response.text), "cartCookie": response.cookies["lc_ebcart"], "sessionId": match.group(1)}
 
 
-
-
-@app.post('/submit')
 def submit(name, additional_access, gtid_list, page_session, lc_ea_po, lc_ebcart):
     url = "https://libcal.library.gatech.edu/ajax/equipment/checkout"
     payload = {'nick': name,

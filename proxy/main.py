4c64f5144cf5463f106e19a6debe8fc2479f7330
Raymond Bian <raybbian@gmail.com>
2023-11-25 00:48:00 -0500

    api updates

--- proxy/main.py
+++ proxy/main.py
@@ -34,7 +34,7 @@ def get_rooms(from_date_string, to_date_string, page_index=0, page_size=18):
 
 
 @app.post('/add-room')
-def add_room(item_id, start_time_urlencoded, checksum, from_date, to_date):
+def add_room(item_id, start_time_urlencoded, duration: int, checksum, from_date, to_date):
     url = "https://libcal.library.gatech.edu/spaces/availability/booking/add"
     payload = f'add%5Beid%5D={item_id}&add%5Bgid%5D={GID}&add%5Blid%5D={LID}&add%5Bstart%5D={start_time_urlencoded}&add%5Bchecksum%5D={checksum}&lid={LID}&gid={GID}&start={from_date}&end={to_date}'
     headers = {
@@ -42,7 +42,17 @@ def add_room(item_id, start_time_urlencoded, checksum, from_date, to_date):
         'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
     }
     response = requests.request("POST", url, headers=headers, data=payload)
-    return json.loads(response.text)
+    data = json.loads(response.text)
+    id = data['bookings'][0]['id']
+    seat_id = data['bookings'][0]['seat_id']
+    new_checksum = data['bookings'][0]['checksum']
+    option_selected = data['bookings'][0]['optionSelected']
+    prev_end = data['bookings'][0]['options'][option_selected].replace(' ', '.')
+    update_end = data['bookings'][0]['options'][duration - 1].replace(' ', '.')
+    update_checksum = data['bookings'][0]['optionChecksums'][duration - 1]
+    payload = f'bookings%5B0%5D%5Bid%5D={id}&bookings%5B0%5D%5Beid%5D={item_id}&bookings%5B0%5D%5Bgid%5D={GID}&bookings%5B0%5D%5Bseat_id%5D={seat_id}&bookings%5B0%5D%5Blid%5D={LID}&bookings%5B0%5D%5Bstart%5D={start_time_urlencoded}&bookings%5B0%5D%5Bend%5D={prev_end}&bookings%5B0%5D%5Bchecksum%5D={new_checksum}&lid={LID}&gid={GID}&start={from_date}&end={to_date}&update%5Bid%5D={id}&update%5Bchecksum%5D={update_checksum}&update%5Bend%5D={update_end}'
+    final_response = requests.request("POST", url, headers=headers, data=payload)
+    return json.loads(final_response.text)
 
 
 @app.post('/create-cart')

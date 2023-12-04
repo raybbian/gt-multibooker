4de36595519f55a3997bf0cb6ef3823db09fec6b
Raymond Bian <raybbian@gmail.com>
2023-12-03 22:40:59 -0500

    fixed spacing

--- ui/src/components/SideBar.js
+++ ui/src/components/SideBar.js
@@ -5,7 +5,16 @@ import {toast} from "react-toastify";
 import {useState} from "react";
 import roomInfo from "../roomInfo/room.definition";
 
-export default function SideBar({rooms, roomID, setRoomID, selections, setSelections, lc_ea_po, getRooms, toggleAuthPanel}) {
+export default function SideBar({
+                                    rooms,
+                                    roomID,
+                                    setRoomID,
+                                    selections,
+                                    setSelections,
+                                    lc_ea_po,
+                                    getRooms,
+                                    toggleAuthPanel
+                                }) {
     const [bookingName, setBookingName] = useState("")
     const [bookingAccess, setBookingAccess] = useState([])
 
@@ -57,114 +66,122 @@ export default function SideBar({rooms, roomID, setRoomID, selections, setSelect
     }
 
     return (
-        <div className={"w-full h-full bg-cream border-r-2 mobile:border-r-0 mobile:border-t-2 border-black flex flex-col justify-between overflow-hidden"}>
-            <div className={"w-full h-full flex flex-col gap-4 overflow-y-scroll p-8"}>
-                <div className={"flex flex-col gap-2"}>
-                    <p className={"text-4xl font-bold"}>GT Multibooker</p>
-                    <p className={"text-base"}>An app to book multiple library rooms at once!</p>
-                </div>
-                <p className={"text-xl font-bold"}>Booking Details:</p>
-                <div className={"flex flex-col gap-2"}>
-                    <select
-                        className={"text-base p-2 border-2 border-black hover:cursor-pointer bg-cream w-full"}
-                        value={roomID}
-                        onChange={(e) => setRoomID(e.target.value)}
-                    >
-                        {Object.keys(roomInfo).sort((a,b) => roomInfo[a].localeCompare(roomInfo[b])).map((option, index) => (
-                            <option key={index} value={option}>
-                                {roomInfo[option]}
-                            </option>
-                        ))}
-                    </select>
-                    <input
-                        className={"w-full bg-cream border-2 border-black p-2 text-base"}
-                        type={"text"}
-                        value={bookingName}
-                        onChange={(e) => setBookingName(e.target.value)}
-                        placeholder={"Booking name"}
-                    />
-                    <form
-                        onSubmit={(e) => {
-                            e.preventDefault()
-                            const newID = e.target[0].value
-                            if (newID.length !== 9) {
-                                toast.error("Invalid GTID!")
-                                return;
-                            }
-                            if (bookingAccess.includes(newID)) {
-                                toast.error("Already given access to this GTID!")
-                                return;
-                            }
-                            e.target[0].value = "";
-                            const updBookingAccess = [...bookingAccess]
-                            updBookingAccess.push(newID)
-                            setBookingAccess(updBookingAccess)
-                        }}
-                    >
-                        <input className={"w-full bg-cream border-2 border-black p-2 text-base"} type={"text"} placeholder={"Additional Access"}/>
-                    </form>
-                    {bookingAccess.map((gtID) => (
-                        <div key={gtID} className={"flex flex-row justify-between w-full h-full border-2 border-black"}>
-                            <div className={"h-full flex"}>
-                                <div className={"w-2 h-full bg-teal"}></div>
-                                <p className={"p-2 ml-2"}>
-                                    {gtID}
-                                </p>
-                            </div>
-                            <div className={"h-full flex p-2 items-center"}>
-                                <button
-                                    onClick={() => {
-                                        const updBookignAccess = bookingAccess.filter((element) => element !== gtID)
-                                        setBookingAccess(updBookignAccess)
-                                    }}>
-                                    <FaTrash className={"hover:text-koi-red"}/>
-                                </button>
-                            </div>
-                        </div>
+        <div
+            className={"w-full h-full flex flex-col gap-4 overflow-y-scroll p-8 bg-cream border-r-2 mobile:border-t-2 mobile:border-r-0 border-black"}>
+            <div className={"flex flex-col gap-2"}>
+                <p className={"text-4xl font-bold"}>GT Multibooker</p>
+                <p className={"text-base"}>An app to book multiple library rooms at once!</p>
+            </div>
+            <p className={"text-xl font-bold"}>Booking Details:</p>
+            <div className={"flex flex-col gap-2"}>
+                <select
+                    className={"text-base p-2 border-2 border-black hover:cursor-pointer bg-cream w-full"}
+                    value={roomID}
+                    onChange={(e) => setRoomID(e.target.value)}
+                >
+                    {Object.keys(roomInfo).sort((a, b) => roomInfo[a].localeCompare(roomInfo[b])).map((option, index) => (
+                        <option key={index} value={option}>
+                            {roomInfo[option]}
+                        </option>
                     ))}
-                </div>
-                <div className={"flex flex-row justify-between items-center"}>
-                    <p className={"text-xl font-bold"}>Bookings:</p>
-                    <div className={"flex flex-row gap-4"}>
-                        <FaKey
-                            size={18}
-                            className={"hover:text-koi-red"}
-                            onClick={() => toggleAuthPanel(true)}
-                        />
-                        <FaTrash
-                            size={18}
-                            className={"hover:text-koi-red"}
-                            onClick={() => setSelections({})}
-                        />
+                </select>
+                <input
+                    className={"w-full bg-cream border-2 border-black p-2 text-base"}
+                    type={"text"}
+                    value={bookingName}
+                    onChange={(e) => setBookingName(e.target.value)}
+                    placeholder={"Booking name"}
+                />
+                <form
+                    onSubmit={(e) => {
+                        e.preventDefault()
+                        const newID = e.target[0].value
+                        if (newID.length !== 9) {
+                            toast.error("Invalid GTID!")
+                            return;
+                        }
+                        if (bookingAccess.includes(newID)) {
+                            toast.error("Already given access to this GTID!")
+                            return;
+                        }
+                        e.target[0].value = "";
+                        const updBookingAccess = [...bookingAccess]
+                        updBookingAccess.push(newID)
+                        setBookingAccess(updBookingAccess)
+                    }}
+                >
+                    <input className={"w-full bg-cream border-2 border-black p-2 text-base"} type={"text"}
+                           placeholder={"Additional Access"}/>
+                </form>
+                {bookingAccess.map((gtID) => (
+                    <div key={gtID} className={"flex flex-row justify-between w-full h-full border-2 border-black"}>
+                        <div className={"h-full flex"}>
+                            <div className={"w-2 h-full bg-teal"}></div>
+                            <p className={"p-2 ml-2"}>
+                                {gtID}
+                            </p>
+                        </div>
+                        <div className={"h-full flex p-2 items-center"}>
+                            <button
+                                onClick={() => {
+                                    const updBookignAccess = bookingAccess.filter((element) => element !== gtID)
+                                    setBookingAccess(updBookignAccess)
+                                }}>
+                                <FaTrash className={"hover:text-koi-red"}/>
+                            </button>
+                        </div>
                     </div>
-                </div>
-                <div className={"flex flex-col"}>
-                    {Object.keys(selections).sort().map((date) => (
-                        <div key={date} className={"flex flex-col gap-2"}>
-                            {selections[date].length > 0 && <p className={"text-base font-semibold mt-2"}>{new Date(date).toDateString()}</p>}
-                            {selections[date].map((booking) => (
-                                <div key={`${booking[0]}-${booking[1]}`} className={"flex flex-row justify-between w-full h-full border-2 border-black"}>
-                                    <div className={"h-full flex"}>
-                                        <div className={"w-2 h-full bg-sakura-pink"}></div>
-                                        <p className={"p-2 ml-2 text-sm"}>
-                                            {getDateTimeFromIdx(booking[0], new Date(date)).toLocaleTimeString('en-US', { hour: "2-digit", minute: "2-digit" })} - {getDateTimeFromIdx(booking[1]+1, new Date(date)).toLocaleTimeString('en-US', { hour: "2-digit", minute: "2-digit" })}
-                                        </p>
-                                    </div>
-                                    <div className={"h-full flex p-2 items-center"}>
-                                        <button
-                                            onClick={() => {
-                                                removeSelection(booking[0], date)
-                                            }}>
-                                            <FaTrash className={"hover:text-koi-red"}/>
-                                        </button>
-                                    </div>
-                                </div>
-                            ))}
-                        </div>
-                    ))}
+                ))}
+            </div>
+            <div className={"flex flex-row justify-between items-center"}>
+                <p className={"text-xl font-bold"}>Bookings:</p>
+                <div className={"flex flex-row gap-4"}>
+                    <FaKey
+                        size={18}
+                        className={"hover:text-koi-red"}
+                        onClick={() => toggleAuthPanel(true)}
+                    />
+                    <FaTrash
+                        size={18}
+                        className={"hover:text-koi-red"}
+                        onClick={() => setSelections({})}
+                    />
                 </div>
             </div>
-            <div className={"flex flex-row justify-end pb-8 pr-8 relative"}>
+            <div className={"flex flex-col"}>
+                {Object.keys(selections).sort().map((date) => (
+                    <div key={date} className={"flex flex-col gap-2"}>
+                        {selections[date].length > 0 &&
+                            <p className={"text-base font-semibold mt-2"}>{new Date(date).toDateString()}</p>}
+                        {selections[date].map((booking) => (
+                            <div key={`${booking[0]}-${booking[1]}`}
+                                 className={"flex flex-row justify-between w-full h-full border-2 border-black"}>
+                                <div className={"h-full flex"}>
+                                    <div className={"w-2 h-full bg-sakura-pink"}></div>
+                                    <p className={"p-2 ml-2 text-sm"}>
+                                        {getDateTimeFromIdx(booking[0], new Date(date)).toLocaleTimeString('en-US', {
+                                            hour: "2-digit",
+                                            minute: "2-digit"
+                                        })} - {getDateTimeFromIdx(booking[1] + 1, new Date(date)).toLocaleTimeString('en-US', {
+                                        hour: "2-digit",
+                                        minute: "2-digit"
+                                    })}
+                                    </p>
+                                </div>
+                                <div className={"h-full flex p-2 items-center"}>
+                                    <button
+                                        onClick={() => {
+                                            removeSelection(booking[0], date)
+                                        }}>
+                                        <FaTrash className={"hover:text-koi-red"}/>
+                                    </button>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                ))}
+            </div>
+            <div className={"flex flex-row justify-end"}>
                 <FaArrowRight
                     className={"hover:text-koi-red"}
                     size={48}
